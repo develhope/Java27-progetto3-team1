800b32139b6b45842f51c211f38f9408694d543c
Federica Parillo <federicaparillo.94@gmail.com>
2024-09-25 15:34:17 +0200

    Inizializzazione Progetto in Spring con aggiunta delle dipendenze

---
+++ src/main/java/com/team1/dealerApp/DealerAppApplication.java
@@ -0,0 +1,13 @@
+package com.team1.dealerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team1/dealerApp/DealerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.dealerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
