f14fc319490d4ce5013be2fe04fa750f13ebbcab
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-03 18:16:50 +0200

    Added mapper class TvShowMapper

---
+++ src/main/java/com/team1/dealerApp/mappers/TvShowMapper.java
@@ -0,0 +1,43 @@
+package com.team1.dealerApp.mappers;
+
+import com.team1.dealerApp.entities.TvShow;
+import com.team1.dealerApp.models.dtos.TvShowDTO;
+
+public class TvShowMapper {
+
+
+    public TvShow toTvShow(TvShowDTO tvShowDTO){
+        return TvShow.builder()
+                .episode(tvShowDTO.getEpisode())
+                .season(tvShowDTO.getSeason())
+                .genre(tvShowDTO.getGenre())
+                .cast(tvShowDTO.getCast())
+                .plot(tvShowDTO.getPlot())
+                .director(tvShowDTO.getDirector())
+                .rating(tvShowDTO.getRating())
+                .title(tvShowDTO.getTitle())
+                .purchasePrice(tvShowDTO.getPurchasePrice())
+                .rentalPrice(tvShowDTO.getRentalPrice())
+                .releaseYear(tvShowDTO.getReleaseYear())
+                .videoStatus(tvShowDTO.getVideoStatus())
+                .build();
+    }
+
+    public TvShowDTO toTvShowDTO(TvShow tvShow){
+        return TvShowDTO.builder()
+                .episode(tvShow.getEpisode())
+                .season(tvShow.getSeason())
+                .genre(tvShow.getGenre())
+                .cast(tvShow.getCast())
+                .plot(tvShow.getPlot())
+                .director(tvShow.getDirector())
+                .rating(tvShow.getRating())
+                .title(tvShow.getTitle())
+                .purchasePrice(tvShow.getPurchasePrice())
+                .rentalPrice(tvShow.getRentalPrice())
+                .releaseYear(tvShow.getReleaseYear())
+                .videoStatus(tvShow.getVideoStatus())
+                .build();
+
+    }
+}
