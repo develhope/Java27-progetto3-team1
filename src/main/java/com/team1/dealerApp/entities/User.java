75bea451b15b921b6e20b476586456e086203e0d
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-10-03 16:54:44 +0200

    Create User Entity with its attributes

---
+++ src/main/java/com/team1/dealerApp/entities/User.java
@@ -0,0 +1,55 @@
+package com.team1.dealerApp.entities;
+
+import com.team1.dealerApp.models.SubsciprionStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private UUID id;
+
+	@Setter
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+
+	@Setter
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+
+	@Setter
+	@Column(nullable = false)
+	private String email;
+
+	@Setter
+	@Column(name = "phone_number", length = 10)
+	private String phoneNumber;
+
+	@Setter
+	@Column(nullable = false)
+	private String password;//ToDo: integrare Spring Security per non salvare password in chiaro
+
+	@Setter
+	@Column(name = "subscription_status")
+	@Enumerated(EnumType.STRING)
+	private SubsciprionStatus subsciprionStatus;
+	/*
+	@Setter
+	@OneToMany()
+	private List<Movie> watchedMovies;
+
+	@Setter
+	@OneToMany
+	private List<TvShow> watchedShows;
+	*/
+}
