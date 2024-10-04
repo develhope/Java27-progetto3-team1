5efeb921faf08a53a765a20c8debe350d3fc0c62
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-04 16:34:25 +0200

    Added endopoint for adding a Movie

--- src/main/java/com/team1/dealerApp/controllers/MovieController.java
+++ src/main/java/com/team1/dealerApp/controllers/MovieController.java
@@ -1,7 +1,9 @@
 package com.team1.dealerApp.controllers;
 
+import com.team1.dealerApp.models.dtos.MovieDTO;
 import com.team1.dealerApp.services.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,7 +13,10 @@ public class MovieController {
 
     private final MovieService movieService;
 
-
+    @PostMapping()
+    public ResponseEntity<MovieDTO> addMovie(@RequestBody MovieDTO movieDTO){
+        return ResponseEntity.status(200).body(movieService.addMovie(movieDTO));
+    }
 
 
 }
