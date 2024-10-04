f7d7434fd5f723d3e24bf7d859bb94a8234e203f
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-04 16:55:29 +0200

    Added endopoint to get all movies

--- src/main/java/com/team1/dealerApp/controllers/MovieController.java
+++ src/main/java/com/team1/dealerApp/controllers/MovieController.java
@@ -3,9 +3,13 @@ package com.team1.dealerApp.controllers;
 import com.team1.dealerApp.models.dtos.MovieDTO;
 import com.team1.dealerApp.services.MovieService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/movie")
@@ -18,10 +22,21 @@ public class MovieController {
         return ResponseEntity.status(200).body(movieService.addMovie(movieDTO));
     }
 
+    // - Modifica film - GetMovieById/GetAllMovies
     @DeleteMapping("/{id}")
     public ResponseEntity<MovieDTO> deleteMovieById(@RequestParam ("id") Long id){
         movieService.deleteMovieById(id);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping()
+    public ResponseEntity<?> getAllMovies(){
+        try{
+            return ResponseEntity.ok(movieService.getAllMovies());
+        } catch (BadRequestException e) {
+            log.error("Error in get all movies {}", e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e);
+        }
     }
 
 }
