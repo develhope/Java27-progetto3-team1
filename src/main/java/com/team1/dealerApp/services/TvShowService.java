75b23c855d70bec2d81c394a8fcd893a080e71b8
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-10-04 17:19:26 +0200

    Add UpdateShowField method for modifying only one field

--- src/main/java/com/team1/dealerApp/services/TvShowService.java
+++ src/main/java/com/team1/dealerApp/services/TvShowService.java
@@ -2,6 +2,7 @@ package com.team1.dealerApp.services;
 
 import com.team1.dealerApp.entities.TvShow;
 import com.team1.dealerApp.mappers.TvShowMapper;
+import com.team1.dealerApp.models.TvShowUpdater;
 import com.team1.dealerApp.models.dtos.TvShowDTO;
 import com.team1.dealerApp.repositories.TvShowRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import java.util.List;
 public class TvShowService {
 	private final TvShowRepository tvShowRepository;
 	private final TvShowMapper tvShowMapper;
+	private final TvShowUpdater<?> tvShowUpdater;
 
 	public List <TvShowDTO> getAllShows () throws BadRequestException {
 		List <TvShow> tvShows = tvShowRepository.findAll();
@@ -39,5 +41,19 @@ public class TvShowService {
 	public TvShowDTO addTvShow (TvShowDTO tvShowDTO){
 		tvShowRepository.save(tvShowMapper.toTvShow(tvShowDTO));
 		return tvShowDTO;
+	}
+
+	public TvShowDTO updateShow (TvShowDTO tvShowDTO, Long id) throws BadRequestException {
+		TvShow found = tvShowMapper.toTvShow(getShowById(id));
+		found = tvShowMapper.toTvShow(tvShowDTO);
+		found.setId(id);
+		tvShowRepository.save(found);
+		return tvShowMapper.toTvShowDTO(found);
+	}
+
+	public TvShowDTO updateShowField (Long id, double newPrice, String field) throws BadRequestException {
+		TvShow tvShow = tvShowRepository
+				.findById(id)
+				.orElseThrow(() -> new BadRequestException("No show with id: " + id))
 	}
 }
