c979896639d01a7dbedbf4d58db0a87d69f97aec
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-10-04 18:16:56 +0200

    add suppress warnings annotation

--- src/main/java/com/team1/dealerApp/controllers/TvShowController.java
+++ src/main/java/com/team1/dealerApp/controllers/TvShowController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@SuppressWarnings("unused")
 @Slf4j
 @RestController
 @RequestMapping("/tvshow")
