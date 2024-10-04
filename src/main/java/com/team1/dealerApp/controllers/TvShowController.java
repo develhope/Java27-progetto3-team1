d854ad1b099bd6f8ad3f7e23d73d4e198cb657ce
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-10-04 18:14:06 +0200

    add updateShow endpoints

--- src/main/java/com/team1/dealerApp/controllers/TvShowController.java
+++ src/main/java/com/team1/dealerApp/controllers/TvShowController.java
@@ -41,10 +41,30 @@ public class TvShowController {
 	}
 
 	@PostMapping()
-	public ResponseEntity<?> addTvShow (@RequestBody TvShowDTO tvShowDTO){
+	public ResponseEntity<TvShowDTO> addTvShow (@RequestBody TvShowDTO tvShowDTO){
+		log.debug("TvShow added successfully in db");
 		return ResponseEntity.ok(tvShowService.addTvShow(tvShowDTO));
 	}
 
+	@PutMapping("{id}")
+	public ResponseEntity<?> updateShow (@RequestBody TvShowDTO tvShowDTO, @PathVariable Long id){
+		try {
+			log.debug("Show updated successfully");
+			return ResponseEntity.ok(tvShowService.updateShow(tvShowDTO, id));
+		}catch ( BadRequestException e ){
+			log.error("Error in updating show with id {}: {}",id, e.getMessage());
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+	}
 
+	@PatchMapping("{id}")
+	public ResponseEntity<?> updateShowField (@PathVariable Long id, @RequestParam(name = "field") String fieldName, @RequestBody Object value){
+		try{
+			return ResponseEntity.ok(tvShowService.updateShowField(id,value, fieldName));
+		} catch ( BadRequestException e ) {
+			log.error("Error in updating product with id {}: {}", id, e.getMessage());
+			return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+	}
 
 }
