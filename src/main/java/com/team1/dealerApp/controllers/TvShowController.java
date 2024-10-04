b1b6609300fa62bfbc74293db82d151accbae2ff
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-10-04 17:55:01 +0200

    add getAllUser Endpoint annotation

--- src/main/java/com/team1/dealerApp/controllers/TvShowController.java
+++ src/main/java/com/team1/dealerApp/controllers/TvShowController.java
@@ -1,9 +1,13 @@
 package com.team1.dealerApp.controllers;
 
 import com.team1.dealerApp.services.TvShowService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class TvShowController {
 
 	private final TvShowService tvShowService;
+
+	@GetMapping
+	public ResponseEntity<?> getAllUsers (){
+		try {
+			return ResponseEntity.ok(tvShowService.getAllShows());
+		}catch ( BadRequestException e ){
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+	}
+
+
+
 
 }
