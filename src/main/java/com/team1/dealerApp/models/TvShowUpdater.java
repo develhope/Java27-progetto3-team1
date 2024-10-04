5d61facc1e8d53cb67e360efd1674fe42dca8aee
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-10-04 17:43:07 +0200

    change return type

--- src/main/java/com/team1/dealerApp/models/TvShowUpdater.java
+++ src/main/java/com/team1/dealerApp/models/TvShowUpdater.java
@@ -2,7 +2,6 @@ package com.team1.dealerApp.models;
 
 import com.team1.dealerApp.entities.TvShow;
 import com.team1.dealerApp.mappers.TvShowMapper;
-import com.team1.dealerApp.models.dtos.TvShowDTO;
 import com.team1.dealerApp.repositories.TvShowRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -14,10 +13,10 @@ public class TvShowUpdater <T>{
 	private final TvShowRepository tvShowRepository;
 	private final TvShowMapper tvShowMapper;
 
-	public TvShowDTO updateShowField( TvShow tvShow, String fieldName, T value) throws NoSuchFieldException, IllegalAccessException {
+	public TvShow updateShowField( TvShow tvShow, String fieldName, T value) throws NoSuchFieldException, IllegalAccessException {
 		Field field = TvShow.class.getField(fieldName);
 		field.setAccessible(true);
 		field.set(tvShow, value );
-		return tvShowMapper.toTvShowDTO(tvShow);
+		return tvShow;
 	}
 }
