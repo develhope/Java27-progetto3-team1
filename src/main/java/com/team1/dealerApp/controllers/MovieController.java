378517a03184709f561ecd2fd571730f635b0308
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-04 17:18:20 +0200

    Added endopoint for get a movie by Id

--- src/main/java/com/team1/dealerApp/controllers/MovieController.java
+++ src/main/java/com/team1/dealerApp/controllers/MovieController.java
@@ -5,9 +5,12 @@ import com.team1.dealerApp.services.MovieService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 @Slf4j
 @RestController
@@ -22,10 +25,10 @@ public class MovieController {
         return ResponseEntity.status(200).body(movieService.addMovie(movieDTO));
     }
 
-    // - Modifica film - GetMovieById/GetAllMovies
-    @DeleteMapping("/{id}")
-    public ResponseEntity<MovieDTO> deleteMovieById(@RequestParam ("id") Long id){
-        movieService.deleteMovieById(id);
+    // - Modifica film
+    @DeleteMapping("/{movieId}")
+    public ResponseEntity<MovieDTO> deleteMovieById(@RequestParam ("movieId") Long movieId){
+        movieService.deleteMovieById(movieId);
         return ResponseEntity.ok().build();
     }
 
@@ -34,8 +37,18 @@ public class MovieController {
         try{
             return ResponseEntity.ok(movieService.getAllMovies());
         } catch (BadRequestException e) {
-            log.error("Error in get all movies {}", e);
+            log.error("Error in get all movies {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e);
+        }
+    }
+
+    @GetMapping("/{movieId}")
+    public ResponseEntity<?> getMovieById(@PathVariable ("movieId") Long movieId){
+        try{
+            return ResponseEntity.ok(movieService.getMovieById(movieId));
+        } catch (NoSuchElementException e){
+            log.error("Error in get movie by Id {} : {}", movieId, e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 
