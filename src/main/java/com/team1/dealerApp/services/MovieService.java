bb3dba6159b20f91aef284ef1c71fdf4e86bee51
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-04 16:54:09 +0200

    Added method to get all movies

--- src/main/java/com/team1/dealerApp/services/MovieService.java
+++ src/main/java/com/team1/dealerApp/services/MovieService.java
@@ -1,11 +1,15 @@
 package com.team1.dealerApp.services;
 
 
+import com.team1.dealerApp.entities.Movie;
 import com.team1.dealerApp.mappers.MovieMapper;
 import com.team1.dealerApp.models.dtos.MovieDTO;
 import com.team1.dealerApp.repositories.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +25,14 @@ public class MovieService {
 
     public void deleteMovieById(Long id) {
         movieRepository.deleteById(id);
+    }
+
+    public List<MovieDTO> getAllMovies() throws BadRequestException {
+        List<Movie> movies = movieRepository.findAll();
+        if(!movies.isEmpty()){
+            return movies.stream().map(movieMapper::toMovieDTO).toList();
+        } else{
+            throw new BadRequestException("There are no film!");
+        }
     }
 }
