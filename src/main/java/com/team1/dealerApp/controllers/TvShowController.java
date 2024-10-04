d2129078730d20eefa24e883a91f3eb44d6f6a4a
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-10-04 18:02:28 +0200

    add addTvShop endpoint

--- src/main/java/com/team1/dealerApp/controllers/TvShowController.java
+++ src/main/java/com/team1/dealerApp/controllers/TvShowController.java
@@ -1,16 +1,16 @@
 package com.team1.dealerApp.controllers;
 
+import com.team1.dealerApp.models.dtos.TvShowDTO;
 import com.team1.dealerApp.services.TvShowService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/tvshow")
 @RequiredArgsConstructor
@@ -23,10 +23,27 @@ public class TvShowController {
 		try {
 			return ResponseEntity.ok(tvShowService.getAllShows());
 		}catch ( BadRequestException e ){
+			log.error("Error in getting users: {}", e.getMessage());
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
 		}
 	}
 
+	@GetMapping("{id}")
+	public ResponseEntity<?> getShowById ( @PathVariable Long id ){
+		try {
+			TvShowDTO tvShowDTO = tvShowService.getShowById(id);
+			log.debug("Found user with id: {}", id);
+			return ResponseEntity.ok(tvShowDTO);
+		}catch ( BadRequestException e ){
+			log.error("Error in getting user: {}", e.getMessage());
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+	}
+
+	@PostMapping()
+	public ResponseEntity<?> addTvShow (@RequestBody TvShowDTO tvShowDTO){
+		return ResponseEntity.ok(tvShowService.addTvShow(tvShowDTO));
+	}
 
 
 
