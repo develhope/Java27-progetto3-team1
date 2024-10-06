3f7ed050d794c4b45c390eb5161f4428cbf9ffec
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-06 18:12:12 +0200

    Add updateMovieField for update only one field

--- src/main/java/com/team1/dealerApp/services/MovieService.java
+++ src/main/java/com/team1/dealerApp/services/MovieService.java
@@ -3,21 +3,25 @@ package com.team1.dealerApp.services;
 
 import com.team1.dealerApp.entities.Movie;
 import com.team1.dealerApp.mappers.MovieMapper;
+import com.team1.dealerApp.models.MovieUpdater;
 import com.team1.dealerApp.models.dtos.MovieDTO;
 import com.team1.dealerApp.repositories.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MovieService {
 
     private final MovieRepository movieRepository;
     private final MovieMapper movieMapper;
+    private final MovieUpdater<Object> movieUpdater;
 
     public MovieDTO addMovie(MovieDTO movieDTO) {
         movieRepository.save(movieMapper.toMovie(movieDTO));
@@ -50,5 +54,22 @@ public class MovieService {
         Movie movieToUpdate = movieRepository.findById(movieId).orElseThrow(()-> new BadRequestException("Impossible to update the movie with id " + movieId ));
         movieRepository.save(movieToUpdate);
         return movieMapper.toMovieDTO(movieToUpdate);
+    }
+
+    public MovieDTO updateMovieField ( Long id, Object value, String field ) throws BadRequestException {
+        Movie tvShow = movieRepository
+                .findById(id)
+                .orElseThrow(() -> new BadRequestException("No movie with id: " + id));
+        Movie updated = new Movie();
+
+        try {
+            updated = movieUpdater.updateMovieField(tvShow, field, value);
+        }catch ( NoSuchFieldException e ){
+            log.error("Error: the field {} does not exist: {}", field, e.getMessage());
+        }catch ( IllegalAccessException e){
+            log.error("Error to access to the class: {}", e.getMessage());
+        }
+        movieRepository.save(updated);
+        return movieMapper.toMovieDTO(updated);
     }
 }
