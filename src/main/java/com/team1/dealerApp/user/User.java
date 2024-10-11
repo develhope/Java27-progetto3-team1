ae27f0f9b64d51420b8952ab1ecc9abfc92c67e4
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-11 16:29:08 +0200

    define mapping for OneToMany relation to rental

--- src/main/java/com/team1/dealerApp/user/User.java
+++ src/main/java/com/team1/dealerApp/user/User.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@SuppressWarnings("unused")
 public class User {
 
 	@Id
@@ -54,7 +55,7 @@ public class User {
 	private List< TvShow > watchedShows;
 
 	@Setter
-	@OneToMany
+	@OneToMany(mappedBy = "renter")
 	private List<Rental> rentals;
 
 	public void setId(UUID id) {
