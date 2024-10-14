0aaaa1e0785d1999a2d14875f68ecfeb03292170
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-14 18:00:46 +0200

    add RentalDTO

---
+++ src/main/java/com/team1/dealerApp/rental/RentalDTO.java
@@ -0,0 +1,29 @@
+package com.team1.dealerApp.rental;
+
+import com.team1.dealerApp.video.movie.Movie;
+import com.team1.dealerApp.video.tvshow.TvShow;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RentalDTO {
+
+    private Long id;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private double rentalPrice;
+    private boolean paid;
+    private List<Movie> movies;
+    private List<TvShow> tvShows;
+    private UUID userId;
+
+
+
+}
