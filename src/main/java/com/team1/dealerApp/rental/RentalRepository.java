45e44cc6eebbf1d1c6dbeeb7beca005be025c40b
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-14 18:08:05 +0200

    Add HQL query for findRenterId

--- src/main/java/com/team1/dealerApp/rental/RentalRepository.java
+++ src/main/java/com/team1/dealerApp/rental/RentalRepository.java
@@ -1,7 +1,17 @@
 package com.team1.dealerApp.rental;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface RentalRepository extends JpaRepository<Rental, Long> { }
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+    @Query("select r from Rental r where r.renter.id = :id")
+    Page<Rental> findByRenterId(UUID id, Pageable pageable);
+
+
+}
