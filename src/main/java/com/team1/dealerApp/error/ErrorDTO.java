125a8c311db00b769404423c94fa214d3f9de2aa
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-17 14:56:02 +0200

    Add class ErrorDTO

---
+++ src/main/java/com/team1/dealerApp/error/ErrorDTO.java
@@ -0,0 +1,12 @@
+package com.team1.dealerApp.error;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorDTO {
+    private String errorMessage;
+}
