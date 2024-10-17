96d7b2954f1ccab81d745cc5c437ae88639d8741
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-17 15:32:52 +0200

    Deleted try-catch, added throws with Exception in the method's signature in RentalController

--- src/main/java/com/team1/dealerApp/rental/RentalController.java
+++ src/main/java/com/team1/dealerApp/rental/RentalController.java
@@ -3,7 +3,7 @@ package com.team1.dealerApp.rental;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,37 +22,23 @@ public class RentalController {
     private final RentalService rentalService;
 
   @PostMapping()
-  public ResponseEntity<?> addRental(@RequestParam (name = "userId") UUID userId, @RequestBody CreateRentalDTO createRentalDTO){
-    try{
+  public ResponseEntity<RentalDTO> addRental(@RequestParam (name = "userId") UUID userId, @RequestBody CreateRentalDTO createRentalDTO) throws BadRequestException{
         return ResponseEntity.ok(rentalService.addRental(userId, createRentalDTO));
-    } catch (BadRequestException e) {
-        log.error("Error in adding a new Rental {} : {}", createRentalDTO, e.getMessage(), e.getStackTrace());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
   }
+
     @GetMapping("/{userId}")
-    public ResponseEntity<?> getAllRentalByUserId(@PathVariable ("userId") UUID userId, @RequestParam (defaultValue = "0") int page, @RequestParam (defaultValue = "10") int size){
-        try{
+    public ResponseEntity<Page<RentalDTO>> getAllRentalByUserId(@PathVariable ("userId") UUID userId, @RequestParam (defaultValue = "0") int page, @RequestParam (defaultValue = "10") int size) throws NoSuchElementException{
             return ResponseEntity.ok(rentalService.getAllRentalByUserId(userId, page, size));
-        } catch (NoSuchElementException e) {
-            log.error("Error in get all rental by user Id {} : {}", userId, e.getMessage(), e.getStackTrace());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
     }
+
     @PatchMapping("/{rentalId}")
-    public ResponseEntity<?> updateRentalEndDate(@PathVariable ("rentalId") Long id, @RequestBody LocalDateTime dateTime){
-        try{
+    public ResponseEntity<RentalDTO> updateRentalEndDate(@PathVariable ("rentalId") Long id, @RequestBody LocalDateTime dateTime) throws NoSuchElementException{
             return ResponseEntity.ok(rentalService.updateRentalEndDate(id, dateTime));
-        } catch (NoSuchElementException e){
-            log.error("Error in update Rental's end date with id {} : {}", id, e.getMessage(), e.getStackTrace());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
     }
 
     @DeleteMapping("/{rentalId}")
-    public ResponseEntity<String> deleteRental(@PathVariable ("rentalId") Long id){
-        rentalService.deleteRental(id);
-        return ResponseEntity.ok("Rental deleted");
+    public ResponseEntity<Boolean> deleteRental(@PathVariable ("rentalId") Long id){
+        return ResponseEntity.ok(rentalService.deleteRental(id));
     }
 
 }
