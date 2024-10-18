f2de984f0143ea5b1afdebcd6b2cebf191823e46
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-18 18:07:34 +0200

    Added log and fix import Slf4j

--- src/main/java/com/team1/dealerApp/admin/AdminController.java
+++ src/main/java/com/team1/dealerApp/admin/AdminController.java
@@ -1,9 +1,8 @@
 package com.team1.dealerApp.admin;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,37 +10,37 @@ import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/admins")
 public class AdminController {
 
-    private static final Logger log = LoggerFactory.getLogger(AdminController.class);
     private final AdminService adminService;
 
 
     @PostMapping()
-    public ResponseEntity<AdminDTO> createAdmin(@RequestBody CreateAdminDTO createAdminDTO) throws BadRequestException{
-            return ResponseEntity.ok(adminService.createAdmin(createAdminDTO));
+    public ResponseEntity<AdminDTO> createAdmin(@RequestBody CreateAdminDTO createAdminDTO) throws BadRequestException {
+        log.debug("Admin created: {}", createAdminDTO);
+        return ResponseEntity.ok(adminService.createAdmin(createAdminDTO));
     }
 
     @GetMapping("/{adminId}")
-    public ResponseEntity<AdminDTO> getAdminById(@PathVariable ("adminId") UUID adminId ) throws NoSuchElementException{
-            return ResponseEntity.ok(adminService.getAdminById(adminId));
+    public ResponseEntity<AdminDTO> getAdminById(@PathVariable("adminId") UUID adminId) throws NoSuchElementException {
+        log.debug("Getting admin with Id {}", adminId);
+        return ResponseEntity.ok(adminService.getAdminById(adminId));
     }
 
     @DeleteMapping("/{adminId}")
-    public ResponseEntity<Boolean> deleteAdmin(@PathVariable ("adminId") UUID adminId){
+    public ResponseEntity<Boolean> deleteAdmin(@PathVariable("adminId") UUID adminId) {
+        log.debug("Deleted admin with Id {}", adminId);
         return ResponseEntity.ok(adminService.deleteAdmin(adminId));
     }
 
     @PutMapping("/{adminId}")
-    public ResponseEntity<AdminDTO> updateAdmin(@PathVariable ("adminId") UUID adminId, @RequestBody CreateAdminDTO createAdminDTO) throws NoSuchElementException{
-            return ResponseEntity.ok(adminService.updateAdmin(adminId,createAdminDTO));
+    public ResponseEntity<AdminDTO> updateAdmin(@PathVariable("adminId") UUID adminId, @RequestBody CreateAdminDTO createAdminDTO) throws NoSuchElementException {
+        log.debug("Updated with Id {}", adminId);
+        return ResponseEntity.ok(adminService.updateAdmin(adminId, createAdminDTO));
     }
-
-
-
-
 
 
 }
