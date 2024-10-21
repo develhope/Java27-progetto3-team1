d352264f134fcab3e6bcbb4c70da76e49b693db3
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-10-21 11:42:11 +0200

    Change List.of in SingletonList for having only one authority per user

--- src/main/java/com/team1/dealerApp/user/User.java
+++ src/main/java/com/team1/dealerApp/user/User.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -68,7 +69,7 @@ public class User implements UserDetails {
 
 	@Override
 	public Collection < ? extends GrantedAuthority > getAuthorities() {
-		return List.of(new SimpleGrantedAuthority(role.name()));
+		return Collections.singletonList(new SimpleGrantedAuthority(role.name()));
 	}
 
 	@Override
