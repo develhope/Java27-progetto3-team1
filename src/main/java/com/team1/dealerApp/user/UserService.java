61e1e5c92fe95d8d1c2a50ce0b9819fc23db7405
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-21 16:32:00 +0200

    Fix return type in registerUser

--- src/main/java/com/team1/dealerApp/user/UserService.java
+++ src/main/java/com/team1/dealerApp/user/UserService.java
@@ -1,9 +1,7 @@
 package com.team1.dealerApp.user;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -44,14 +42,15 @@ public class UserService {
     }
 
 
-    public User registerUser(CreateUserDTO userDTO) throws BadRequestException {
+    public UserDTO registerUser(CreateUserDTO userDTO) throws BadRequestException {
         User user = userMapper.toUser(userDTO);
         if(userRepository.existsByEmail(user.getEmail())) {
             throw new BadRequestException("Email already exists");
         }
         // Crittografia della password
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+         userRepository.save(user);
+         return userMapper.toUserDTO(user);
     }
 
     public User getUserById(UUID id) throws NoSuchElementException{
