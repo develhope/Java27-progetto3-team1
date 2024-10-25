10c989b29dca18bf811df173d5fbf575629593fa
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-25 18:45:12 +0200

    Fix method's arguments and rentalRepository methods to call

--- src/main/java/com/team1/dealerApp/rental/RentalService.java
+++ src/main/java/com/team1/dealerApp/rental/RentalService.java
@@ -8,6 +8,7 @@ import org.apache.coyote.BadRequestException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -21,7 +22,7 @@ public class RentalService {
     private final RentalMapper rentalMapper;
     private final UserService userService;
 
-    public RentalDTO addRental(UUID userId, CreateRentalDTO createRentalDTO) throws BadRequestException {
+    public RentalDTO addRental(UserDetails user, CreateRentalDTO createRentalDTO) throws BadRequestException {
         if(createRentalDTO.getMovies().isEmpty() && createRentalDTO.getTvShows().isEmpty()){
             throw new BadRequestException("Both lists can not be empty");
         }
@@ -32,23 +33,23 @@ public class RentalService {
         userRental.setRentalPrice(totalPrice);
         userRental.setStartDate(LocalDateTime.now());
         userRental.setEndDate(LocalDateTime.now().plusDays(14));
-        userRental.setRenter(userService.getUserById(userId));
+        userRental.setRenter(userService.getUserByEmail(user));
         userRental.setRentalStatus(RentalStatus.ACTIVE);
         rentalRepository.save(userRental);
         return rentalMapper.toDTO(userRental);
 
     }
 
-    public Page<RentalDTO> getAllRentalByUserId(UUID userId, int page, int size) throws NoSuchElementException {
+    public Page<RentalDTO> getActiveUserRentals(UserDetails user, int page, int size) throws NoSuchElementException {
         Pageable pageable = PageRequest.of(page, size);
-        Page<Rental> rentalsFind = rentalRepository.findByRenterId(userId,pageable);
+        Page<Rental> rentalsFind = rentalRepository.findByRenter_Email(user.getUsername(), pageable);
         if(rentalsFind.isEmpty()){
             throw new NoSuchElementException("Rental's list is empty");
         }
         return rentalsFind.map(rentalMapper::toDTO);
     }
-    public RentalDTO updateRentalEndDate(Long id, LocalDateTime dateTime) {
-        Rental rentalFound = rentalRepository.findById(id).orElseThrow(()-> new NoSuchElementException("There is no rental with id " + id));
+    public RentalDTO updateRentalEndDate(UserDetails user, Long id, LocalDateTime dateTime) {
+        Rental rentalFound = rentalRepository.findByRenter_EmailAndId(user.getUsername(), id).orElseThrow(()-> new NoSuchElementException("There is no rental with id " + id));
         rentalFound.setEndDate(dateTime);
         rentalRepository.save(rentalFound);
         return rentalMapper.toDTO(rentalFound);
