29e327b13fe96ad332b876cd272de41519715b5f
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-25 18:47:50 +0200

    Fix endpoint

--- src/main/java/com/team1/dealerApp/purchase/PurchaseController.java
+++ src/main/java/com/team1/dealerApp/purchase/PurchaseController.java
@@ -1,14 +1,17 @@
 package com.team1.dealerApp.purchase;
 
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
-import java.util.UUID;
+
 
 @Slf4j
 @RestController
@@ -19,35 +22,23 @@ public class PurchaseController {
     private final PurchaseService purchaseService;
 
     @PostMapping()
-    public ResponseEntity<?> addPurchase(@RequestParam(name = "userId") UUID userId, @RequestBody CreatePurchaseDTO createPurchaseDTO) {
-        try {
-            return ResponseEntity.ok(purchaseService.addPurchase(userId, createPurchaseDTO));
-        } catch (BadRequestException e) {
-            log.error("Error in adding a new Purchase {} : {}", createPurchaseDTO, e.getMessage(), e.getStackTrace());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
+    public ResponseEntity<PurchaseDTO> addPurchase(@AuthenticationPrincipal UserDetails user, @RequestBody CreatePurchaseDTO createPurchaseDTO) throws BadRequestException {
+        return ResponseEntity.ok(purchaseService.addPurchase(user, createPurchaseDTO));
+
     }
 
 
     @GetMapping("/{userId}")
-    public ResponseEntity<?> getPurchaseByUserId(@PathVariable ("userId") UUID userId, @RequestParam (defaultValue = "0") int page, @RequestParam (defaultValue = "10") int size) {
-        try{
-            return ResponseEntity.ok(purchaseService.getPurchaseByUserId(userId, page, size));
-        } catch (NoSuchElementException e){
-            log.error("Error in get purchase by UserId {} : {}", userId, e.getMessage(), e.getStackTrace());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+    public ResponseEntity<Page<PurchaseDTO>> getUserPurchase(@AuthenticationPrincipal UserDetails user, @RequestParam (defaultValue = "0") int page, @RequestParam (defaultValue = "10") int size) throws NoSuchElementException{
+        return ResponseEntity.ok(purchaseService.getPurchaseByUserId(user, page, size));
+
     }
 
 
     @PutMapping("/{purchaseId}")
-    public ResponseEntity<?> updatePurchase(@PathVariable ("purchaseId") Long id, @RequestBody CreatePurchaseDTO createPurchaseDTO) {
-        try {
-            return ResponseEntity.ok(purchaseService.updatePurchase(id, createPurchaseDTO));
-        } catch (NoSuchElementException e){
-            log.error("Error in update Purchase with id {} : {}", id, e.getMessage(), e.getStackTrace());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+    public ResponseEntity<?> updatePurchase(@PathVariable ("purchaseId") Long id, @RequestBody CreatePurchaseDTO createPurchaseDTO) throws NoSuchElementException {
+        return ResponseEntity.ok(purchaseService.updatePurchase(id, createPurchaseDTO));
+
     }
 
 
