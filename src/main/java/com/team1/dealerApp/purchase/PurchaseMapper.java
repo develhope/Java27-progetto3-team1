68e0d30fd89c6ab906189b2f43d023419d19ddce
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-25 20:14:14 +0200

    Fix purchaseMapper

--- src/main/java/com/team1/dealerApp/purchase/PurchaseMapper.java
+++ src/main/java/com/team1/dealerApp/purchase/PurchaseMapper.java
@@ -1,14 +1,24 @@
 package com.team1.dealerApp.purchase;
 
+import com.team1.dealerApp.video.movie.Movie;
+import com.team1.dealerApp.video.movie.MovieMapper;
+import com.team1.dealerApp.video.tvshow.TvShow;
+import com.team1.dealerApp.video.tvshow.TvShowMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-@Component
-public class PurchaseMapper {
+import java.util.List;
 
-    public Purchase toPurchase(CreatePurchaseDTO createPurchaseDTO) {
+@Component
+@RequiredArgsConstructor
+public class PurchaseMapper {
+    private final MovieMapper movieMapper;
+    private final TvShowMapper tvShowMapper;
+
+    public Purchase toPurchase(CreatePurchaseDTO createPurchaseDTO, List<Movie> movieList, List<TvShow> tvShowsList) {
         return Purchase.builder()
-                .movies(createPurchaseDTO.getMovies())
-                .tvShows(createPurchaseDTO.getTvShows())
+                .movies(movieList)
+                .tvShows(tvShowsList)
                 .build();
     }
 
@@ -16,9 +26,10 @@ public class PurchaseMapper {
         return PurchaseDTO.builder()
                 .id(purchase.getId())
                 .purchasePrice(purchase.getPurchasePrice())
-                .movies(purchase.getMovies())
-                .tvShows(purchase.getTvShows())
+                .movies(purchase.getMovies().stream().map(movieMapper::toMovieDTO).toList())
+                .tvShows(purchase.getTvShows().stream().map(tvShowMapper::toTvShowDTO).toList())
                 .userId(purchase.getPurchaser().getId())
+                .orderStatus(purchase.getOrderStatus())
                 .build();
     }
 }
