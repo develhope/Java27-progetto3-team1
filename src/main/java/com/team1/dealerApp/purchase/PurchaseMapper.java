80c8b6daf635ec79b481047eaaa062e8b8410c84
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-25 20:18:20 +0200

    Add SuppressWarnings

--- src/main/java/com/team1/dealerApp/purchase/PurchaseMapper.java
+++ src/main/java/com/team1/dealerApp/purchase/PurchaseMapper.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@SuppressWarnings("unused")
 public class PurchaseMapper {
     private final MovieMapper movieMapper;
     private final TvShowMapper tvShowMapper;
