155a6e9c4b8842fb17d31b00fb31eecc79614230
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-10-28 17:59:51 +0100

    - Refactor addRental for better readeability - Add role check in total price calculation

--- src/main/java/com/team1/dealerApp/rental/RentalService.java
+++ src/main/java/com/team1/dealerApp/rental/RentalService.java
@@ -1,5 +1,7 @@
 package com.team1.dealerApp.rental;
 
+import com.team1.dealerApp.user.Role;
+import com.team1.dealerApp.user.User;
 import com.team1.dealerApp.user.UserService;
 import com.team1.dealerApp.video.movie.Movie;
 import com.team1.dealerApp.video.tvshow.TvShow;
@@ -12,53 +14,81 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class RentalService {
-    private final RentalRepository rentalRepository;
-    private final RentalMapper rentalMapper;
-    private final UserService userService;
+	private final RentalRepository rentalRepository;
+	private final RentalMapper rentalMapper;
+	private final UserService userService;
 
-    public RentalDTO addRental(UserDetails user, CreateRentalDTO createRentalDTO) throws BadRequestException {
-        if (createRentalDTO.getMovies().isEmpty() && createRentalDTO.getTvShows().isEmpty()) {
-            throw new BadRequestException("Both lists can not be empty");
-        }
-        Rental userRental = rentalMapper.toRental(createRentalDTO);
-        Double movieRentalPrice = createRentalDTO.getMovies().stream().mapToDouble(Movie::getRentalPrice).sum();
-        Double showRentalPrice = createRentalDTO.getTvShows().stream().mapToDouble(TvShow::getRentalPrice).sum();
-        Double totalPrice = movieRentalPrice + showRentalPrice;
-        userRental.setRentalPrice(totalPrice);
-        userRental.setStartDate(LocalDateTime.now());
-        userRental.setEndDate(LocalDateTime.now().plusDays(14));
-        userRental.setRenter(userService.getUserByEmail(user));
-        userRental.setRentalStatus(RentalStatus.ACTIVE);
-        rentalRepository.save(userRental);
-        return rentalMapper.toDTO(userRental);
+	public RentalDTO addRental( UserDetails user, CreateRentalDTO createRentalDTO ) throws BadRequestException {
+		validateRentalRequest(createRentalDTO);
 
-    }
+		List < Movie > movies = fetchMovies(createRentalDTO);
+		List < TvShow > tvShows = fetchTvShows(createRentalDTO);
+		User renter = userService.getUserByEmail(user);
+		double totalPrice = calculateTotalRentalPrice(movies, tvShows, renter.getRole());
 
-    public Page<RentalDTO> getActiveUserRentals(UserDetails user, int page, int size) throws NoSuchElementException {
-        Pageable pageable = PageRequest.of(page, size);
-        Page<Rental> rentalsFind = rentalRepository.findByRenter_Email(user.getUsername(), pageable);
-        if (rentalsFind.isEmpty()) {
-            throw new NoSuchElementException("Rental's list is empty");
-        }
-        return rentalsFind.map(rentalMapper::toDTO);
-    }
+		Rental rental = createRental(user, createRentalDTO, totalPrice);
+		rentalRepository.save(rental);
 
-    public RentalDTO updateRentalEndDate(UserDetails user, Long id, LocalDateTime dateTime) {
-        Rental rentalFound = rentalRepository.findByRenter_EmailAndId(user.getUsername(), id).orElseThrow(() -> new NoSuchElementException("There is no rental with id " + id));
-        rentalFound.setEndDate(dateTime);
-        rentalRepository.save(rentalFound);
-        return rentalMapper.toDTO(rentalFound);
-    }
+		return rentalMapper.toDTO(rental);
+	}
 
-    public boolean deleteRental(Long id) {
-        rentalRepository.deleteById(id);
-        return true;
-    }
+	private void validateRentalRequest( CreateRentalDTO createRentalDTO ) throws BadRequestException {
+		if ( createRentalDTO.getMovies().isEmpty() && createRentalDTO.getTvShows().isEmpty() ) {
+			throw new BadRequestException("Both lists cannot be empty");
+		}
+	}
+
+	private List < Movie > fetchMovies( CreateRentalDTO createRentalDTO ) {
+		return createRentalDTO.getMovies().isEmpty()? List.of(): createRentalDTO.getMovies();
+	}
+
+	private List < TvShow > fetchTvShows( CreateRentalDTO createRentalDTO ) {
+		return createRentalDTO.getTvShows().isEmpty()? List.of(): createRentalDTO.getTvShows();
+	}
+
+	private double calculateTotalRentalPrice( List < Movie > movies, List < TvShow > tvShows, Role role ) {
+		double movieRentalPrice = movies.stream().mapToDouble(Movie::getRentalPrice).sum();
+		double tvShowRentalPrice = tvShows.stream().mapToDouble(TvShow::getRentalPrice).sum();
+		if ( role.equals(Role.ROLE_TVSHOWS) ) tvShowRentalPrice = 0;
+		if ( role.equals(Role.ROLE_MOVIES) ) movieRentalPrice = 0;
+		return movieRentalPrice + tvShowRentalPrice;
+	}
+
+	private Rental createRental( UserDetails user, CreateRentalDTO createRentalDTO, double totalPrice ) {
+		Rental rental = rentalMapper.toRental(createRentalDTO);
+		rental.setRentalPrice(totalPrice);
+		rental.setStartDate(LocalDateTime.now());
+		rental.setEndDate(LocalDateTime.now().plusDays(14));
+		rental.setRenter(userService.getUserByEmail(user));
+		rental.setRentalStatus(RentalStatus.ACTIVE);
+		return rental;
+	}
+
+	public Page < RentalDTO > getActiveUserRentals( UserDetails user, int page, int size ) throws NoSuchElementException {
+		Pageable pageable = PageRequest.of(page, size);
+		Page < Rental > rentalsFind = rentalRepository.findByRenter_Email(user.getUsername(), pageable);
+		if ( rentalsFind.isEmpty() ) {
+			throw new NoSuchElementException("Rental's list is empty");
+		}
+		return rentalsFind.map(rentalMapper::toDTO);
+	}
+
+	public RentalDTO updateRentalEndDate( UserDetails user, Long id, LocalDateTime dateTime ) {
+		Rental rentalFound = rentalRepository.findByRenter_EmailAndId(user.getUsername(), id).orElseThrow(() -> new NoSuchElementException("There is no rental with id " + id));
+		rentalFound.setEndDate(dateTime);
+		rentalRepository.save(rentalFound);
+		return rentalMapper.toDTO(rentalFound);
+	}
+
+	public boolean deleteRental( Long id ) {
+		rentalRepository.deleteById(id);
+		return true;
+	}
 
 }
