1712f4352a5e4eb9e89b20058f66c3488435663f
Federica Parillo <federicaparillo.94@gmail.com>
2024-11-05 00:09:49 +0100

    Fix

--- src/main/java/com/team1/dealerApp/subscription/SubscriptionMapper.java
+++ src/main/java/com/team1/dealerApp/subscription/SubscriptionMapper.java
@@ -1,7 +1,10 @@
 package com.team1.dealerApp.subscription;
 
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+@Component
 @SuppressWarnings("unused")
 public class SubscriptionMapper {
 
