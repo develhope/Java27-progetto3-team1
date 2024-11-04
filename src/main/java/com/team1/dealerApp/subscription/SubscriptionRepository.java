385dcc901e30a6d50301bbf4369cfdf986a00fff
Federica Parillo <federicaparillo.94@gmail.com>
2024-11-04 23:47:01 +0100

    Add findByStatus and findBySubscriptionType in SubscriptionRepository

--- src/main/java/com/team1/dealerApp/subscription/SubscriptionRepository.java
+++ src/main/java/com/team1/dealerApp/subscription/SubscriptionRepository.java
@@ -3,7 +3,13 @@ package com.team1.dealerApp.subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+
+    List<Subscription> findByStatus(Boolean status);
+    List<Subscription> findBySubscriptionType(SubscriptionType subscriptionType);
+
 
 }
