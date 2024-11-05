3ca2f0a99b005f02100d8b3f8e98007942c1bde5
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-11-05 16:20:12 +0100

    Optimize import

--- src/main/java/com/team1/dealerApp/rental/RentalController.java
+++ src/main/java/com/team1/dealerApp/rental/RentalController.java
@@ -5,9 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
--- src/main/java/com/team1/dealerApp/video/tvshow/TvShowRepository.java
+++ src/main/java/com/team1/dealerApp/video/tvshow/TvShowRepository.java
@@ -1,6 +1,5 @@
 package com.team1.dealerApp.video.tvshow;
 
-import com.team1.dealerApp.video.movie.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
