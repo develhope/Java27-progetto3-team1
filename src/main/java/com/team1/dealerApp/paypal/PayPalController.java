dc8014760fafdd07de0429c13352798cf1c54c7c
Federica Parillo <federicaparillo.94@gmail.com>
2024-11-07 16:45:58 +0100

    Fix endpoint for Purchase Add endpoint for Rental

--- src/main/java/com/team1/dealerApp/paypal/PayPalController.java
+++ src/main/java/com/team1/dealerApp/paypal/PayPalController.java
@@ -2,6 +2,8 @@ package com.team1.dealerApp.paypal;
 
 import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
+import com.team1.dealerApp.purchase.PurchaseService;
+import com.team1.dealerApp.rental.RentalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,17 +18,37 @@ import org.springframework.web.bind.annotation.RestController;
 public class PayPalController {
 
 	private final PayPalService payPalService;
+	private final PurchaseService purchaseService;
+	private final RentalService rentalService;
 
-	@GetMapping( "/success" )
-	public ResponseEntity < ? > successPayment( @RequestParam( "paymentId" ) String paymentId, @RequestParam( "PayerID" ) String payerId ) throws PayPalRESTException {
+	@GetMapping( "/success/purchase" )
+	public ResponseEntity < ? > successPayment( @RequestParam( "paymentId" ) String paymentId, @RequestParam( "PayerID" ) String payerId, @RequestParam ("orderId") Long orderId) throws PayPalRESTException {
 
 		// Esegui il pagamento con l'ID ricevuto
 		Payment payment = payPalService.executePayment(paymentId, payerId);
 
 		if ( "approved".equals(payment.getState()) ) {
+			purchaseService.updatePurchaseStatus(orderId, "paid");
 			// Se il pagamento è approvato, ritorna una conferma
 			return ResponseEntity.ok("Pagamento completato con successo");
 		} else {
+			purchaseService.updatePurchaseStatus(orderId, "canceled");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Pagamento non approvato");
+		}
+	}
+
+	@GetMapping( "/success/rental" )
+	public ResponseEntity < ? > successRentalPayment( @RequestParam( "paymentId" ) String paymentId, @RequestParam( "PayerID" ) String payerId, @RequestParam ("orderId") Long orderId) throws PayPalRESTException {
+
+		// Esegui il pagamento con l'ID ricevuto
+		Payment payment = payPalService.executePayment(paymentId, payerId);
+
+		if ( "approved".equals(payment.getState()) ) {
+			rentalService.updateRentalStatus(orderId, "active");
+			// Se il pagamento è approvato, ritorna una conferma
+			return ResponseEntity.ok("Pagamento completato con successo");
+		} else {
+			rentalService.updateRentalStatus(orderId, "suspended");
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Pagamento non approvato");
 		}
 	}
