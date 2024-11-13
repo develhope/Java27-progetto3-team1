c59c62e7bbb0ba7ddcf669539e3e1e2be77a8b53
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-11-13 19:52:37 +0100

    Add VideoEditDTO class

---
+++ src/main/java/com/team1/dealerApp/video/VideoEditDTO.java
@@ -0,0 +1,13 @@
+package com.team1.dealerApp.video;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class VideoEditDTO {
+	String field;
+	Object value;
+}
