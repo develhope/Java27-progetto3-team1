ad51b19fecbce327810961ede0d1cdbb47d5dedc
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-11-14 10:41:15 +0100

    Add Map in updateMovieField RequestBody

--- src/main/java/com/team1/dealerApp/video/movie/MovieController.java
+++ src/main/java/com/team1/dealerApp/video/movie/MovieController.java
@@ -1,6 +1,5 @@
 package com.team1.dealerApp.video.movie;
 
-import com.team1.dealerApp.video.VideoEditDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
@@ -9,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @Slf4j
@@ -52,8 +52,8 @@ public class MovieController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/a/movies/{movieId}")
-    public ResponseEntity<MovieDTO> updateMovieField( @PathVariable Long movieId, @RequestBody VideoEditDTO videoEditDTO) throws BadRequestException, NoSuchFieldException, IllegalAccessException {
-        return ResponseEntity.ok(movieService.updateMovieField(videoEditDTO, movieId));
+    public ResponseEntity<MovieDTO> updateMovieField( @PathVariable Long movieId, @RequestBody Map <String, Object> fieldValueMap) throws BadRequestException, NoSuchFieldException, IllegalAccessException {
+        return ResponseEntity.ok(movieService.updateMovieField(fieldValueMap, movieId));
     }
 
     @GetMapping("/a/movies/sales/{movieId}")
